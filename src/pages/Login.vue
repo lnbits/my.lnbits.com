0d7eb09a6220710faeaf9ddc0011e5ef14f862a0
Vlad Stan <stan.v.vlad@gmail.com>
2024-04-30 16:06:28 +0300

    chore: icon stuff

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -5,9 +5,9 @@
         <q-card
           v-bind:style="q.screen.lt.sm ? { width: '80%' } : { width: '30%' }"
         >
-          <q-card-section>
-            <q-avatar size="103px" class="absolute-center shadow-10">
-              <img src="profile.svg" />
+          <q-card-section class="q-mb-md">
+            <q-avatar size="150px" class="absolute-center shadow-10 " color="primary">
+              <img src="profile.svg" class="q-pa-sm" />
             </q-avatar>
           </q-card-section>
           <q-card-section>
