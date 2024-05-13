ec39cf927c76563daa7e884c36c80bc72b1b9e24
dni ⚡ <office@dnilabs.com>
2024-05-13 11:58:44 +0200

    start on port

--- quasar.config.js
+++ quasar.config.js
@@ -86,7 +86,8 @@ module.exports = configure(function (/* ctx */) {
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#devServer
     devServer: {
       // https: true
-      open: true // opens browser window automatically
+      open: true, // opens browser window automatically
+      port: 8080,
     },
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#framework
