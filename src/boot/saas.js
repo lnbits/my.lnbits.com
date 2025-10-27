d55a327a9b2ea97eb82ef3fe028bf579167ebf9a
Vlad Stan <stan.v.vlad@gmail.com>
2025-10-27 21:33:45 +0200

    Revert "feat: add subscription plans (#22)"
    This reverts commit cc01576ce1dc1bf68314a2431fe0d9a83a6c3731.

--- src/boot/saas.js
+++ src/boot/saas.js
@@ -1,215 +1,144 @@
-import axios from 'axios'
-import {secondsToDhm} from 'src/boot/utils'
+import axios from "axios";
+import { secondsToDhm } from "src/boot/utils";
 
 var saas = {
-  slideimg: 'assets/images/hero/bitcoin-accounts.png',
-  url: 'https://api.lnbits.com',
+  slideimg: "assets/images/hero/bitcoin-accounts.png",
+  url: "https://api.lnbits.com",
   // for local development togegther with Caddy
-  // url: '/api',
+  // url: "/api",
   serverTime: null,
 
-  username: localStorage.getItem('username'),
+  username: localStorage.getItem("username"),
 
   signup: async function (username, password, password2) {
-    const {data} = await axios({
-      method: 'POST',
-      url: this.url + '/signup',
+    const { data } = await axios({
+      method: "POST",
+      url: this.url + "/signup",
       withCredentials: true,
       data: {
         username,
         password,
-        password_repeat: password2
-      }
-    })
+        password_repeat: password2,
+      },
+    });
 
-    localStorage.setItem('username', username)
+    localStorage.setItem("username", username);
 
-    return data
+    return data;
   },
   login: async function (username, password) {
-    const formData = new FormData()
-    formData.append('username', username)
-    formData.append('password', password)
-    const {data} = await axios({
-      method: 'POST',
-      url: this.url + '/login',
+    const formData = new FormData();
+    formData.append("username", username);
+    formData.append("password", password);
+    const { data } = await axios({
+      method: "POST",
+      url: this.url + "/login",
       data: formData,
       withCredentials: true,
       headers: {
-        'Content-Type': 'multipart/form-data'
-      }
-    })
-    localStorage.setItem('username', username)
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    localStorage.setItem("username", username);
 
-    return data
+    return data;
   },
 
   createInstance: async function () {
     return axios({
-      method: 'POST',
-      url: this.url + '/instance',
-      withCredentials: true
-    })
+      method: "POST",
+      url: this.url + "/instance",
+      withCredentials: true,
+    });
   },
 
   updateInstance: function (id, action) {
     return axios({
-      method: 'PUT',
-      url: this.url + '/instance',
+      method: "PUT",
+      url: this.url + "/instance",
       withCredentials: true,
       data: {
         action: action,
-        instance_id: id
-      }
-    })
-  },
-
-  createOneTimePlan: function (instanceId, planName, quantity, isFiat) {
-    return axios({
-      method: 'PUT',
-      url: this.url + '/instance/buy',
-      withCredentials: true,
-      data: {
-        instance_id: instanceId,
-        quantity: quantity,
-        payment_plan_name: planName,
-        is_fiat: isFiat
-      }
-    })
-  },
-  subscribeToPlan: function (instanceId, planName) {
-    return axios({
-      method: 'PUT',
-      url: this.url + '/instance/subscribe',
-      withCredentials: true,
-      data: {
-        instance_id: instanceId,
-        payment_plan_name: planName
-      }
-    })
-  },
-  unsubscribe: function (instanceId, subscriptionRequestId) {
-    return axios({
-      method: 'PUT',
-      url: this.url + '/instance/unsubscribe',
-      withCredentials: true,
-      data: {
-        instance_id: instanceId,
-        subscription_request_id: subscriptionRequestId
-      }
-    })
+        instance_id: id,
+      },
+    });
   },
   getInstances: async function () {
     const response = await axios({
-      method: 'GET',
-      url: this.url + '/instance',
-      withCredentials: true
-    })
-    return response
+      method: "GET",
+      url: this.url + "/instance",
+      withCredentials: true,
+    });
+
+    return response;
   },
   getUserInstancesLogs: async function () {
     const response = await axios({
-      method: 'GET',
-      url: this.url + '/instance/logs',
-      withCredentials: true
-    })
+      method: "GET",
+      url: this.url + "/instance/logs",
+      withCredentials: true,
+    });
 
-    return response
+    return response;
   },
   getInstancesLogs: async function (id) {
     const response = await axios({
-      method: 'GET',
+      method: "GET",
       url: this.url + `/instance/${id}/logs`,
-      withCredentials: true
-    })
+      withCredentials: true,
+    });
 
-    return response
-  },
-  getUserPayments: async function () {
-    const response = await axios({
-      method: 'GET',
-      url: this.url + '/instance/payments',
-      withCredentials: true
-    })
-
-    return response
-  },
-  getInstancePayments: async function (id) {
-    const response = await axios({
-      method: 'GET',
-      url: this.url + `/instance/${id}/payments`,
-      withCredentials: true
-    })
-
-    return response
-  },
-  getUserSubscriptions: async function () {
-    const response = await axios({
-      method: 'GET',
-      url: this.url + '/instance/subscriptions',
-      withCredentials: true
-    })
-
-    return response
-  },
-  getInstanceSubscriptions: async function (id) {
-    const response = await axios({
-      method: 'GET',
-      url: this.url + `/instance/${id}/subscriptions`,
-      withCredentials: true
-    })
-
-    return response
+    return response;
   },
   status: async function () {
     const response = await axios({
-      method: 'GET',
+      method: "GET",
       url: this.url,
-      withCredentials: true
-    })
+      withCredentials: true,
+    });
 
-    this.serverTime = response.data.timestamp
+    this.serverTime = response.data.timestamp;
 
-    return response
+    return response;
   },
   logout: async function () {
     const response = await axios({
-      method: 'POST',
-      url: this.url + '/logout',
-      withCredentials: true
-    })
-    this.username = null
-    localStorage.clear()
-    return response
+      method: "POST",
+      url: this.url + "/logout",
+      withCredentials: true,
+    });
+    this.username = null;
+    localStorage.clear();
+    return response;
   },
 
   mapInstance: function (instance) {
     const progress = (start, stop, serverTime) => {
       if (!serverTime) {
-        return 0
+        return 0;
       }
       if (stop - start <= 0 || stop - serverTime <= 0) {
-        return 100
+        return 100;
       }
 
-      const percentage = (1 - (stop - serverTime) / (stop - start)) * 100
+      const percentage = (1 - (stop - serverTime) / (stop - start)) * 100;
 
-      return Math.floor(percentage)
-    }
+      return Math.floor(percentage);
+    };
 
     const status = (active, enabled) => {
-      if (!active) {
-        return 'Not Paid'
+      if (!active){
+        return "Not Paid"
       }
       if (!enabled) {
-        return 'Disabled'
+        return "Disabled"
       }
-      return 'Running'
+      return "Running"
     }
 
     const timeLeft = Math.floor(
       Math.max(instance.timestamp_stop - this.serverTime, 0)
-    )
+    );
     return {
       id: instance.id,
       instanceLink: `https://${instance.domain}/wallet`,
@@ -231,34 +160,34 @@ var saas = {
         instance.timestamp_start || instance.timestamp,
         instance.timestamp_stop,
         this.serverTime
-      )
-    }
+      ),
+    };
   },
   mapErrorToString(error) {
-    const data = error.response?.data
+    const data = error.response?.data;
     if (!data) {
-      return
+      return;
     }
-    if (typeof data === 'string') {
-      return data
+    if (typeof data === "string") {
+      return data;
     }
-    return data?.detail?.map(d => d.msg).join(', ')
-  }
-}
+    return data?.detail?.map((d) => d.msg).join(", ");
+  },
+};
 
-;(async () => {
+(async () => {
   axios.interceptors.response.use(
-    response => response,
-    err => {
+    (response) => response,
+    (err) => {
       if (err?.response?.status === 401) {
-        saas.logout()
-        if (window.location.pathname !== '/login') {
-          setTimeout(() => (window.location.href = '/login'), 500)
+        saas.logout();
+        if (window.location.pathname !==  "/login") {
+          setTimeout(() => (window.location.href = "/login"), 500);
         }
       }
-      return Promise.reject(err)
+      return Promise.reject(err);
     }
-  )
-})()
+  );
+})();
 
-export {saas}
+export { saas };
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,37 +1,31 @@
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
     children: [
-      {path: '', component: () => import('pages/Instances.vue')},
-      {
-        path: '/instances',
-        component: () => import('pages/Instances.vue'),
-        props: route => ({plan: route.query.plan})
-      },
-      {path: '/activity', component: () => import('pages/Activity.vue')},
-      {path: '/subscriptions', component: () => import('pages/Subscriptions.vue')},
-      {path: '/payments', component: () => import('pages/Payments.vue')},
+      { path: "", component: () => import("pages/Instances.vue") },
+      { path: "/instances", component: () => import("pages/Instances.vue") },
+      { path: "/activity", component: () => import("pages/Activity.vue") },
 
       // Not completed yet
       // {path: '/Taskboard', component: () => import('pages/TaskBoard.vue')},
-    ]
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/Error404.vue')
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/Error404.vue"),
   },
   {
-    path: '/pricing',
-    component: () => import('pages/Pricing.vue')
+    path: "/pricing",
+    component: () => import("pages/Pricing.vue"),
   },
   {
-    path: '/login',
-    component: () => import('pages/Login.vue')
-  }
-]
+    path: "/login",
+    component: () => import("pages/Login.vue"),
+  },
+];
 
-export default routes
+export default routes;
