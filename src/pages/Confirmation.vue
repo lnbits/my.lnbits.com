388497b9ba9ae0cf29363b00589000f057a072da
Vlad Stan <stan.v.vlad@gmail.com>
2026-02-25 14:32:42 +0200

    feat: go to confirm after signup

---
+++ src/pages/Confirmation.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-layout>
+    <q-page-container>
+      <q-page class="flex bg-image flex-center">
+        <q-card v-bind:style="q.screen.lt.sm ? {width: '80%'} : {width: '30%'}">
+          <q-card-section class="text-center q-pa-xl text-h6">
+            The confirmation email has been sent.
+          </q-card-section>
+          <q-card-section class="text-center  text-h6">
+            Please check your inbox.
+          </q-card-section>
+        </q-card>
+      </q-page>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import {defineComponent} from 'vue'
+import {useQuasar} from 'quasar'
+
+export default defineComponent({
+  setup() {
+    const $q = useQuasar()
+    return {
+      q: $q
+    }
+  }
+})
+</script>
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -219,7 +219,9 @@ export default defineComponent({
           message: 'Signed Up!',
           color: 'positive'
         })
-        setTimeout(() => (window.location.href = '/'), 500)
+        setTimeout(() => {
+          this.$router.push('/confirm')
+        }, 500)
       } catch (error) {
         console.warn(error)
         this.q.notify({
